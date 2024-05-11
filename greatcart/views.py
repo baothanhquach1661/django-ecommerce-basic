9f57c1ec3522194736f2e535608d7c2410b6f761
Bao T Quach <baothanhquach1661@gmail.com>
2024-05-10 18:20:28 -0700

    First Commit

---
+++ greatcart/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from store.models import Product
+from store.models import Category
+
+
+def home(request):
+    products = Product.objects.all().filter(is_available=True)
+
+    categories = Category.objects.all()
+
+    context = {
+        'products': products,
+        'categories': categories,
+    }
+
+    return render(request, 'home.html', context)
